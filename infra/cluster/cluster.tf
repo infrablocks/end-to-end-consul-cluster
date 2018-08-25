46ad016d6bfc2d2f2077ba4e6da8703cca91debf
Toby Clemson <tobyclemson@gmail.com>
2018-08-25 15:50:46 +0100

    Add cluster.

---
+++ infra/cluster/cluster.tf
@@ -0,0 +1,19 @@
+module "ecs_cluster" {
+  source = "infrablocks/ecs-cluster/aws"
+  version = "0.2.7-rc.2"
+
+  region = "${var.region}"
+  vpc_id = "${data.terraform_remote_state.tooling_network.vpc_id}"
+  subnet_ids = "${data.terraform_remote_state.tooling_network.private_subnet_ids}"
+  allowed_cidrs = ["${var.private_network_cidr}"]
+
+  component = "${var.component}"
+  deployment_identifier = "${var.deployment_identifier}"
+
+  cluster_instance_ssh_public_key_path = "${var.cluster_instance_ssh_public_key_path}"
+  cluster_instance_type = "${var.cluster_instance_type}"
+
+  cluster_minimum_size = 2
+  cluster_maximum_size = 4
+  cluster_desired_capacity = 3
+}
